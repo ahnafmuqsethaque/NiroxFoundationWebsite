bb3cbc09121ca8e334d1ee67cfd040e4e7b68279
Ahnaf Muqset Haque <ahnafmuqset@gmail.com>
2020-07-22 14:05:53 -0700

    added routing funcitonality

---
+++ src/components/Reservations.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Reservations() {
+
+    return(
+        <div>
+            <h1>Reservations</h1>
+        </div>
+    )
+
+}
+
+export default Reservations; 
