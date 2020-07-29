334b5733257ee74c0dee6867075bcc6eca24e539
laurajank2 <laura_jankowski@brown.edu>
2020-07-29 11:04:28 -0700

    member info carosel update

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,22 +6,76 @@ import "../styles/home.css";
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <div class="card" id="memberinfo">
-        <img
-          src="https://static.boredpanda.com/blog/wp-content/uploads/2016/02/cute-baby-polar-bear-day-photography-14__880.jpg"
-          class="card-img-top"
-          alt="..."
-        />
-        <div class="card-body">
-          <h5 class="card-title">Membership Info</h5>
-          <p class="card-text">
-            Some quick info about what you get with your membership.
-          </p>
-          <a href="#" class="btn btn-primary">
-            More Info (goes to membership page)
-          </a>
+      <div
+        id="carouselExampleCaptions"
+        class="carousel slide"
+        data-ride="carousel"
+      >
+        <ol class="carousel-indicators">
+          <li
+            data-target="#carouselExampleCaptions"
+            data-slide-to="0"
+            class="active"
+          ></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active img-fluid">
+            <img
+              src="https://live.staticflickr.com/7433/10953992113_1a85d64982_b.jpg"
+              id="first"
+              class="d-block w-100"
+              alt="..."
+            />
+            <div class="carousel-caption d-md-block">
+              <h5>Member Info Fact 1</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://live.staticflickr.com/7433/10953992113_1a85d64982_b.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
+            <div class="carousel-caption d-md-block">
+              <h5>Member Info Fact 2</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://live.staticflickr.com/7433/10953992113_1a85d64982_b.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
+            <div class="carousel-caption d-md-block">
+              <h5>Member Info Fact 3</h5>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </div>
+          </div>
         </div>
+        <a
+          class="carousel-control-prev"
+          href="#carouselExampleCaptions"
+          role="button"
+          data-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a
+          class="carousel-control-next"
+          href="#carouselExampleCaptions"
+          role="button"
+          data-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
       </div>
       <h2>Events</h2>
       <div
