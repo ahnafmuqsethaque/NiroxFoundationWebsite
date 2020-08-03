cf16727ce4d340c69ca77f25059be437720bdef5
Ahnaf Muqset Haque <ahnafmuqset@gmail.com>
2020-08-03 13:21:17 -0700

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ class Header extends Component {
                         <section class="row no-gutters align-items-center">
                             <div class="col-12 text-center d-flex">
                                 <img class="img-fluid of" src="https://images.unsplash.com/photo-1504853823479-80156a5aebb4?ixlib=rb-0.3.5&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;s=ff3076b05d9cb00914ce076bd2beedee&amp;auto=format&amp;fit=crop&amp;w=1900&amp;h=400&amp;q=80" />
-                                <h4 class="w-50 position-absolute text-danger mt-4 mr-5 pr-5">This is also<br />available on your mobile.</h4>
+                                <h4 class="w-50 position-absolute text-danger mt-4 mr-5 pr-5">We can put other<br />text right here</h4>
                             </div>
                         </section>
                     </div>
@@ -27,7 +27,7 @@ class Header extends Component {
                 </div> */}
 
 
-                <nav class="navbar navbar-light bg-light">
+                <nav class="navbar navbar-light bg-secondary">
                     <div class="col-sm-3">
 
                         <div class="row">
